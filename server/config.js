b6150d1ffaaae89c17493611062a3861eb39acd9
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-30 00:26:13 +0100

    Delete server directory

--- server/config.js
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-
-export default {
-    JWT_SECRET : process.env.JWT_SECRET,
-    EMAIL: process.env.EMAIL, 
-    PASSWORD : process.env.PASSWORD,
-    ATLAS_URI: process.env.ATLAS_URI || "your_mongodb_atlas_url<its_very_imprtant>"
-}
