3915ad36b070b28e371d55fcdda45c63a9e7da4f
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-11-29 01:35:59 +0100

    Add files via upload

---
+++ server/config.js
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export default {
+    JWT_SECRET : process.env.JWT_SECRET,
+    EMAIL: process.env.EMAIL, 
+    PASSWORD : process.env.PASSWORD,
+    ATLAS_URI: process.env.ATLAS_URI || "your_mongodb_atlas_url<its_very_imprtant>"
+}
