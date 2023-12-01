c9d965cca1456d24bcb3a05d27fc3231565dd649
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-12-01 01:17:36 +0100

    Update Password.js

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -17,7 +17,7 @@ export default function Password() {
 
   const formik = useFormik({
     initialValues : {
-      password : 'admin@123'
+      password : ''
     },
     validate : passwordValidate,
     validateOnBlur: false,
@@ -51,8 +51,8 @@ export default function Password() {
         <div className={styles.glass}>
 
           <div className="title flex flex-col items-center">
-            <h4 className='text-5xl font-bold'>Hello {apiData?.firstName || apiData?.username}</h4>
-            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+            <h4 className='text-xl font-bold'>Hello {apiData?.firstName || apiData?.username}</h4>
+            <span className='py-4 w-2/3 text-center text-gray-500'>
               Explore More by connecting with us.
             </span>
           </div>
@@ -62,12 +62,12 @@ export default function Password() {
                   <img src={apiData?.profile || avatar} className={styles.profile_img} alt="avatar" />
               </div>
 
-              <div className="textbox flex flex-col items-center gap-6">
+              <div className="textbox flex flex-col items-center gap-4">
                   <input {...formik.getFieldProps('password')} className={styles.textbox} type="text" placeholder='Password' />
                   <button className={styles.btn} type='submit'>Sign In</button>
               </div>
 
-              <div className="text-center py-4">
+              <div className="text-center py-2">
                 <span className='text-gray-500'>Forgot Password? <Link className='text-red-500' to="/recovery">Recover Now</Link></span>
               </div>
 
