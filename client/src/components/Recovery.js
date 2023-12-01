e09a1e2c68a7b76e3f9bc13d2a3c5c37163d84c5
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-12-01 01:39:55 +0100

    Update Recovery.js

--- client/src/components/Recovery.js
+++ client/src/components/Recovery.js
@@ -71,7 +71,7 @@ export default function Recovery() {
               <div className="textbox flex flex-col items-center gap-4">
 
                   <div className="input text-center">
-                    <span className='py-8 text-sm text-left text-gray-500'>
+                    <span className='my-8 text-sm text-left text-gray-500'>
                       Enter 6 digit OTP sent to your email address.
                     </span>
                     <input onChange={(e) => setOTP(e.target.value) } className={styles.textbox} type="text" placeholder='OTP' />
