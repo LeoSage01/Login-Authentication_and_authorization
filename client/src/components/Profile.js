716d39fff4ee7f1ece6f64e74707c6ab02c8ab02
Ugwuoke Victor <113137578+LeoSage01@users.noreply.github.com>
2023-12-01 01:19:04 +0100

    Update Profile.js

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -63,11 +63,11 @@ export default function Profile() {
       <Toaster position='top-center' reverseOrder={false}></Toaster>
 
       <div className='flex justify-center items-center h-screen'>
-        <div className={`${styles.glass} ${extend.glass}`} style={{ width: "45%", paddingTop: '3em'}}>
+        <div className={`${styles.glass} ${extend.glass}`} style={{ width: "45%", paddingTop: '2em'}}>
 
           <div className="title flex flex-col items-center">
-            <h4 className='text-5xl font-bold'>Profile</h4>
-            <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
+            <h4 className='text-xl font-bold'>Profile</h4>
+            <span className='py-4 w-2/3 text-center text-gray-500'>
                 You can update the details.
             </span>
           </div>
@@ -81,13 +81,13 @@ export default function Profile() {
                   <input onChange={onUpload} type="file" id='profile' name='profile' />
               </div>
 
-              <div className="textbox flex flex-col items-center gap-6">
-                <div className="name flex w-3/4 gap-10">
+              <div className="textbox flex flex-col items-center gap-4">
+                <div className="name flex w-3/4 gap-6">
                   <input {...formik.getFieldProps('firstName')} className={`${styles.textbox} ${extend.textbox}`} type="text" placeholder='FirstName' />
                   <input {...formik.getFieldProps('lastName')} className={`${styles.textbox} ${extend.textbox}`} type="text" placeholder='LastName' />
                 </div>
 
-                <div className="name flex w-3/4 gap-10">
+                <div className="name flex w-3/4 gap-6">
                   <input {...formik.getFieldProps('mobile')} className={`${styles.textbox} ${extend.textbox}`} type="text" placeholder='Mobile No.' />
                   <input {...formik.getFieldProps('email')} className={`${styles.textbox} ${extend.textbox}`} type="text" placeholder='Email*' />
                 </div>
